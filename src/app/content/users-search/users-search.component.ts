825194bf6d68c069525bd03b9793ec44b7b96649
sumatokhinsa <c.c@bk.ru>
2018-05-15 14:14:04 +0300

    new component and fix redirect

---
+++ src/app/content/users-search/users-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-search',
+  templateUrl: './users-search.component.html',
+  styleUrls: ['./users-search.component.styl']
+})
+export class UsersSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
