bc7b435fca647d884eccc94f67c5f70f417ecaef
sumatokhinsa <c.c@bk.ru>
2018-06-05 09:56:32 +0300

    rxJs fromEvent

--- src/app/services/activity-user.service.ts
+++ src/app/services/activity-user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { takeWhile } from 'rxjs/operators';
+import { fromEvent } from 'rxjs/observable/fromEvent';
 import { Router } from '@angular/router';
 import { LocalStorage } from '@ngx-pwa/local-storage';
 
@@ -20,17 +20,17 @@ export class ActivityUserService {
 
   idleLogout() {
     let t;
-    const resetTimer = _ => {
+    const resetTimer = () => {
       clearTimeout( t );
       t = setTimeout( _ => { this.logout(); }, this.getMinutes( 15 ) );  // time is in milliseconds
     };
 
-    window.onload = resetTimer;
-    window.onmousemove = resetTimer;
-    window.onmousedown = resetTimer; // catches touchscreen presses
-    window.onclick = resetTimer;     // catches touchpad clicks
-    window.onscroll = resetTimer;    // catches scrolling with arrow keys
-    window.onkeypress = resetTimer;
+    fromEvent(document, 'load').subscribe( _ => resetTimer() );
+    fromEvent(document, 'mousemove').subscribe( _ => resetTimer() );
+    fromEvent(document, 'mousedown').subscribe( _ => resetTimer() );
+    fromEvent(document, 'click').subscribe( _ => resetTimer() );
+    fromEvent(document, 'scroll').subscribe( _ => resetTimer() );
+    fromEvent(document, 'keypress').subscribe( _ => resetTimer() );
   }
 
 
